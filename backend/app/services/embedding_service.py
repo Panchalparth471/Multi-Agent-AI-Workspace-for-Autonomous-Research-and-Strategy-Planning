4c4ee9ba55c4ed4acabffffc7418c18cc9aff97a
Zynox The Dev <116634521+Panchalparth471@users.noreply.github.com>
2025-06-18 19:49:44 +0530

    Delete backend directory

--- backend/app/services/embedding_service.py
+++
@@ -1,5 +0,0 @@
-from langchain_cohere import CohereEmbeddings
-import os
-
-def get_embedding_model():
-    return CohereEmbeddings(cohere_api_key=os.getenv("COHERE_API_KEY"), model="embed-english-v3.0" )
--- backend/app/tools/document_loader.py
+++
@@ -1,7 +0,0 @@
-from langchain.document_loaders import TextLoader
-from langchain.schema import Document
-from typing import List
-
-def load_documents_from_texts(texts: List[str]) -> List[Document]:
-    return [Document(page_content=text) for text in texts]
-
